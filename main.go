628b489b5c4a8df6df1706162df2f84fdc772778
kaitoyama <uitakato@gmail.com>
2025-03-20 00:21:43 +0900

    refactor: use CHECKIN_PORT environment variable for server port configuration

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/traPtitech/Checkin-Server/router"
 	"go.uber.org/zap"
-)
-
-var (
-	port = 3000
 )
 
 // App holds all the dependencies for our application
@@ -29,6 +27,11 @@ func NewApp(logger *zap.Logger, handlers *router.Handlers) *App {
 
 // Start begins the HTTP server
 func (a *App) Start() error {
+	portStr := os.Getenv("CHECKIN_PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		a.Logger.Fatal("failed to parse CHECKIN_PORT", zap.Error(err))
+	}
 	a.Logger.Info("starting server", zap.Int("port", port))
 	if err := a.Server.Start(fmt.Sprintf(":%d", port)); err != nil {
 		a.Logger.Info("shutting down the server")
