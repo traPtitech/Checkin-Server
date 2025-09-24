b12001408c19d77fa142fcec929d064f3fecacce
uni-kakurenbo <kohma1116@gmail.com>
2025-09-24 13:51:16 +0900

    feat: router に各エンドポイントを登録し， Not implemented を返す

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/traPtitech/Checkin-Server
 
-go 1.23.3
+go 1.23.4
+
+toolchain go1.24.4
 
 require (
 	github.com/labstack/echo/v4 v4.13.3
@@ -9,11 +11,23 @@ require (
 )
 
 require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/getkin/kin-openapi v0.128.0 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/invopop/yaml v0.3.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/oapi-codegen/runtime v1.1.1 // indirect
+	github.com/perimeterx/marshmallow v1.1.5 // indirect
+	github.com/traPtitech/Checkin-openapi v0.0.0-20250101104207-adaf6a7f63c2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
@@ -21,4 +35,5 @@ require (
 	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/traPtitech/Checkin-Server/repository"
+	api "github.com/traPtitech/Checkin-openapi/server"
 	"go.uber.org/zap"
 )
 
@@ -11,7 +12,59 @@ type Handlers struct {
 	Repo   repository.Repository
 }
 
+// DeleteAdmin implements api.ServerInterface.
+func (h *Handlers) DeleteAdmin(ctx echo.Context, params api.DeleteAdminParams) error {
+	return NotImplementedError()
+}
+
+// GetAdmins implements api.ServerInterface.
+func (h *Handlers) GetAdmins(ctx echo.Context) error {
+	return NotImplementedError()
+}
+
+// PostAdmin implements api.ServerInterface.
+func (h *Handlers) PostAdmin(ctx echo.Context) error {
+	return NotImplementedError()
+}
+
+// GetCustomer implements api.ServerInterface.
+func (h *Handlers) GetCustomer(ctx echo.Context, params api.GetCustomerParams) error {
+	return NotImplementedError()
+}
+
+// PatchCustomer implements api.ServerInterface.
+func (h *Handlers) PatchCustomer(ctx echo.Context) error {
+	return NotImplementedError()
+}
+
+// PostCustomer implements api.ServerInterface.
+func (h *Handlers) PostCustomer(ctx echo.Context) error {
+	return NotImplementedError()
+}
+
+// PostInvoice implements api.ServerInterface.
+func (h *Handlers) PostInvoice(ctx echo.Context) error {
+	return NotImplementedError()
+}
+
+// GetCheckoutSessions implements api.ServerInterface.
+func (h *Handlers) GetCheckoutSessions(ctx echo.Context, params api.GetCheckoutSessionsParams) error {
+	return NotImplementedError()
+}
+
+// GetInvoices implements api.ServerInterface.
+func (h *Handlers) GetInvoices(ctx echo.Context, params api.GetInvoicesParams) error {
+	return NotImplementedError()
+}
+
+// PostWebhookInvoicePaid implements api.ServerInterface.
+func (h *Handlers) PostWebhookInvoicePaid(ctx echo.Context, params api.PostWebhookInvoicePaidParams) error {
+	return NotImplementedError()
+}
+
 func (h *Handlers) Setup() *echo.Echo {
 	e := echo.New()
+	api.RegisterHandlers(e, h)
+
 	return e
 }
